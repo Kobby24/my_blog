588d1566618264185723cab2b6542e667a69947a
Kobby24 <geffah005@st.ug.edu.gh>
2025-02-13 12:34:14 +0000

    models added to admin site

--- BLOG-POST/blog/admin.py
+++ BLOG-POST/blog/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
+from .models import *
 
 
 # Register your models here.
 
 
+admin.site.register(AuthUser)
+admin.site.register(BlogPost)
+admin.site.register(AuthGroup)
+admin.site.register(AuthGroupPermissions)
+admin.site.register(AuthPermission)
+admin.site.register(AuthUserGroups)
+admin.site.register(AuthUserUserPermissions)
+admin.site.register(DjangoAdminLog)
+admin.site.register(DjangoContentType)
+admin.site.register(DjangoMigrations)
+admin.site.register(DjangoSession)
