87dc9f36dcd52b33a9412622f488b953caf09954
Kobby24 <geffah005@st.ug.edu.gh>
2025-02-16 12:28:17 +0000

    add congrats page

--- BLOG-POST/blog/views.py
+++ BLOG-POST/blog/views.py
@@ -22,17 +22,6 @@ def about(request):
     return render(request, 'about.html')
 
 
-def contact(request):
-    if request.method == "POST":
-        send_message(name=request.POST.get('username')
-                     , phone=request.POST.get("phone"),
-                     user_mail=request.POST.get('email'),
-                     message=request.POST.get("message"))
-
-
-    return render(request, 'contact.html')
-
-
 def admins(request):
     if request.method == "POST":
         form = Login(request.POST)
