4619876941d57c75facb0c12775b82f3de1622f7
Kobby24 <geffah005@st.ug.edu.gh>
2025-02-16 11:19:06 +0000

    post page done

--- BLOG-POST/blog/utils.py
+++ BLOG-POST/blog/utils.py
@@ -1,7 +1,7 @@
 from .models import *
 
 def get_post(post_id):
-    post = BlogPost.objects.get(blog_id=post_id.blog_id)
+    post = BlogPost.objects.get(blog_id=post_id)
     return post
 
 def get_all_post():
@@ -9,5 +9,6 @@ def get_all_post():
     blog_posts = []
 
     for post in posts:
-        blog_posts.append(get_post(post))
+        blog_posts.append(get_post(post.blog_id))
     return blog_posts
+
