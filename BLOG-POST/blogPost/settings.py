fbc413bff5c3d991bc3cda066a8bfd2c5b07db57
Kobby24 <geffah005@st.ug.edu.gh>
2025-02-16 18:23:43 +0000

    deployment underway

--- BLOG-POST/blogPost/settings.py
+++ BLOG-POST/blogPost/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = 'django-insecure-!ep)(f%@gvbcray3!(5vgp@hhmn#jw$h15op59k_9crt@fprwh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-CSRF_TRUSTED_ORIGINS = []
+ALLOWED_HOSTS = ['myblog-production-8531.up.railway.app',"https://myblog-production-8531.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ['myblog-production-8531.up.railway.app',"https://myblog-production-8531.up.railway.app"]
 
 # Application definition
 
