5ab509f620a8f6963604eedf49a0a28bc16f0e8a
Kobby24 <geffah005@st.ug.edu.gh>
2025-03-01 23:10:25 +0000

    like button

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,26 @@
+import uuid
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import Login, CreatePost
 from .utils import *
 
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
+from .models import BlogPost,Like  # Replace with your actual model
+
+from django.http import JsonResponse
 
 def home(request):
     post = get_all_post()
 
     event = get_events()
+    response = render(request, 'index.html', {"data": post,'event':event})
 
-    return render(request, 'index.html', {"data": post,'event':event})
+    anonymous_user_id = get_anonymous_user_id(request)
+    response.set_cookie('anonymous_user_id', anonymous_user_id, max_age=31536000)
+
+    return response
 
 
 def article(request, p_id):
@@ -50,53 +61,40 @@ def admins(request):
     return render(request, 'login.html', {'form': form})
 
 
-def create(request):
+
+
+def get_anonymous_user_id(request):
+    anonymous_user_id = request.COOKIES.get('anonymous_user_id')
+    if not anonymous_user_id:
+        anonymous_user_id = str(uuid.uuid4())
+
+    return anonymous_user_id
+
+
+
+@csrf_exempt
+def like_post(request, post_id):
     if request.method == "POST":
-        form = CreatePost(request.POST)
+        post = get_object_or_404(BlogPost, blog_id=post_id)
 
-        if form.is_valid():
-            post = {
-                'title': form.cleaned_data['title'],
-                'subtitle': form.cleaned_data['subtitle'],
-                'body': form.cleaned_data['body'],
-                'url': form.cleaned_data['url']
-            }
+        # Get anonymous user ID from cookies
+        anonymous_user_id = get_anonymous_user_id(request)
 
-            return home(request)
-        else:
-            return HttpResponse(status=404)
-    else:
-        form = CreatePost()
-        return render(request, 'createPost.html', {'form': form, 'mode': 'create'})
+        # Check if this user has already liked the post
+        if Like.objects.filter(anonymous_user_id=anonymous_user_id, post=post).exists():
+            return JsonResponse({"status": "already_liked", "like_count": post.like_count})
 
-# def delete(request, p_id):
-#     if None:
-#         post = None
-#         return render(request, 'index.html', {"data": post})
-#     else:
-#         return HttpResponse(status=404)
+        # Increase like count
+        post.like_count += 1
+        post.save()
+
+        # Save the like in the database
+        Like.objects.create(anonymous_user_id=anonymous_user_id, post=post)
+
+        return JsonResponse({"status": "liked", "like_count": post.like_count})
+
+    return JsonResponse({"status": "error"}, status=400)
 
 
-# def update(request, p_id):
-#     post = None
-#     if request.method == 'POST':
-#         form = CreatePost(request.POST)
-#         if form.is_valid():
-#             article_body = {
-#                 'title': form.cleaned_data['title'],
-#                 'subtitle': form.cleaned_data['subtitle'],
-#                 'url': form.cleaned_data['url'],
-#                 'body': form.cleaned_data['body']
-#             }
-#
-#             return home(request)
-#
-#     else:
-#         form = CreatePost(initial={
-#             'title': post['title'],
-#             'subtitle': post['subtitle'],
-#             'url': post['url'],
-#             'body': post['body']
-#         })
-#
-#     return render(request, 'createPost.html', {'form': form, 'mode': 'update','p_id':p_id})
+
+
