1eb4e331077b7278032cf1c39f60402857833231
Kobby24 <geffah005@st.ug.edu.gh>
2025-03-04 23:53:29 +0000

    like button

--- blog/utils.py
+++ blog/utils.py
@@ -54,5 +54,5 @@ def get_founder():
 
 
 def about_background():
-    image = AboutPageBackground.objects.all().lastest('image_id')
+    image = AboutPageBackground.objects.all().latest('image_id')
     return image
